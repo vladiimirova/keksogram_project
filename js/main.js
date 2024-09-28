d404f67c0f324ad566c6bf276cbba7850bf67e67
Vladimirova Tetiana <vladimirovatanya1107@gmail.com>
2024-09-29 01:27:19 +0300

    add module1-task3

--- js/main.js
+++ js/main.js
@@ -1,59 +1,81 @@
-import { photosArr, messages, names, likes, comments, avatar, id, randomWords } from './constants.js';
-import { getRandomNumber, getRandomEl } from './utils.js';
-import { generateAllMiniatures } from './miniature.js';
-  
-  function getAvatar() {
-    return `img/avatar-${getRandomNumber(avatar.min, avatar.max)}.svg`;
-  }
+import {
+  photosArr,
+  messages,
+  names,
+  likes,
+  comments,
+  avatar,
+  id,
+  randomWords,
+} from "./constants.js";
+import { getRandomNumber, getRandomEl } from "./utils.js";
+import { generateAllMiniatures } from "./miniature.js";
+import { showBigPicture, closePicture } from "./full-size.js";
 
-  function generateComment() {
-    return {
-      id: getRandomNumber(id.min, id.max),
-      avatar: getAvatar(), 
-      message: getRandomEl(messages), 
-      name: getRandomEl(names)
-    }
-  }
-
-  function generateComments() {
-    const commentsCount = getRandomNumber(comments.min, comments.max);
-
-    return Array(commentsCount).fill(null).map(generateComment);
-  }
-
-  function generateDescription(arr) {
-    const shuffled = arr.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, 3).join(' '); 
+function getAvatar() {
+  return `img/avatar-${getRandomNumber(avatar.min, avatar.max)}.svg`;
 }
 
-// photosArr.forEach((el, i) => {
-//   el = {
-//     id: i + 1, 
-//     url: 'photos/' + (i + 1) + '.jpg', 
-//     description: 'Опис фотографії номер ' + (i + 1), 
-//     likes: getRandomNumber(likes.min, likes.max), 
-//     comments: generateComments() 
-//   };
-// });
+function generateComment() {
+  return {
+    id: getRandomNumber(id.min, id.max),
+    avatar: getAvatar(),
+    message: getRandomEl(messages),
+    name: getRandomEl(names),
+  };
+}
 
-    photosArr.forEach((_, i) => {
-      photosArr[i] = { 
-        id: i + 1, 
-        url: 'photos/' + (i + 1) + '.jpg', 
-        description: generateDescription(randomWords), 
-        likes: getRandomNumber(likes.min, likes.max), 
-        comments: generateComments() 
-      };
-    });
-    
-  
-  console.log(JSON.stringify(photosArr, null, 2));
+function generateComments() {
+  const commentsValue = getRandomNumber(comments.min, comments.max);
 
-  const pictures = document.querySelector('.pictures');
-  const fragment = document.createDocumentFragment();
-  
-  generateAllMiniatures(photosArr).forEach(miniature => {
-      fragment.appendChild(miniature);
-  });
-  
-  pictures.appendChild(fragment);
+  return Array(commentsValue).fill(null).map(generateComment);
+}
+
+function generateDescription(arr) {
+  const shuffled = arr.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, 3).join(" ");
+}
+
+photosArr.forEach((_, i) => {
+  photosArr[i] = {
+    id: i + 1,
+    url: "photos/" + (i + 1) + ".jpg",
+    description: generateDescription(randomWords),
+    likes: getRandomNumber(likes.min, likes.max),
+    comments: generateComments(),
+  };
+});
+
+console.log(JSON.stringify(photosArr, null, 2));
+
+const pictures = document.querySelector(".pictures");
+const fragment = document.createDocumentFragment();
+
+generateAllMiniatures(photosArr).forEach((miniature) => {
+  fragment.appendChild(miniature);
+});
+
+pictures.appendChild(fragment);
+
+const pictureSection = document.querySelector(".pictures");
+
+pictureSection.addEventListener("click", (e) => {
+  const id = +e.target.dataset.id;
+  const selectedPhoto = photosArr.find((photo) => photo.id === id);
+  if (selectedPhoto) {
+    showBigPicture(selectedPhoto);
+  }
+});
+
+const bigPicture = document.querySelector(".big-picture");
+
+const closeButton = bigPicture.querySelector(".big-picture__cancel");
+closeButton.addEventListener("click", (e) => {
+  closePicture();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closePicture();
+  }
+});
--- js/miniature.js
+++ js/miniature.js
@@ -1,10 +1,16 @@
- function generateSingleMiniature(photoData) {
+function generateSingleMiniature(photoData) {
     const pictureTemplate = document.querySelector('#picture').content.cloneNode(true);
-    pictureTemplate.querySelector('img').src = photoData.url;
-    pictureTemplate.querySelector('.picture__comments').innerText = photoData.comments.length;
-    pictureTemplate.querySelector('.picture__likes').innerText = photoData.likes;
-    return pictureTemplate;
+    const imgElement = pictureTemplate.querySelector('img');
+
+    imgElement.src = photoData.url; 
+    imgElement.dataset.id = photoData.id;
+    
+    pictureTemplate.querySelector('.picture__comments').innerText = photoData.comments.length; 
+    pictureTemplate.querySelector('.picture__likes').innerText = photoData.likes; 
+    
+    return pictureTemplate; 
 }
+
 
 export function generateAllMiniatures(photosArr) {
     return photosArr.map(photo => generateSingleMiniature(photo));
